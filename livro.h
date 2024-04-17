67eb76bb5b5a1ddf6f60352b48e8a66d520fa4a9
guilhermef2k <48020094+guilhermef2k@users.noreply.github.com>
2024-04-17 19:58:28 -0300

    Update livro.h

--- livro.h
+++ livro.h
@@ -1,1 +1,37 @@
+#ifndef H_BIBLIOTECA
+#define H_BIBLIOTECA
 
+typedef struct livro;
+typedef struct login;
+typedef struct conta;
+
+void menuinicial();
+
+void criarconta();
+
+int verificaEmail(char *email);
+
+void logar();
+
+void cadastrar();
+
+void listar();
+
+int tamanho();
+
+int tamanho2();
+
+void editar(int codigo);
+
+void excluir(int codigo);
+
+void buscar (int cod);
+
+void catalogos();
+
+void emprestimo(int cod);
+
+void meusEmprestimos();
+
+void ordenar();
+#endif
